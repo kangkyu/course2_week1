f2c7b99d60ba9518496aab2497e58c9c2e951b01
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-06-23 18:57:48 -0700

    Add seed data file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "jimmy")
+User.first.posts.create(title: "Google", url: "google.com", description: "A famous search engine")
